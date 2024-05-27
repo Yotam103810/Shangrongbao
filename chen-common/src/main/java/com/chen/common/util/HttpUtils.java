e8c0e07865131984ddb5c5423f966f0df88a3a9d
Yotam103810 <15590774401@163.com>
2024-05-27 23:10:37 +0800

    dev第三次提交，添加HttpUtils工具类

---
+++ chen-common/src/main/java/com/chen/common/util/HttpUtils.java
@@ -0,0 +1,83 @@
+package com.chen.common.util;
+
+import lombok.extern.slf4j.Slf4j;
+
+import java.io.*;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLConnection;
+
+@Slf4j
+public class HttpUtils {
+    static final String POST = "POST";
+    static final String GET = "GET";
+    static final int CONN_TIMEOUT = 30000;// ms
+    static final int READ_TIMEOUT = 30000;// ms
+
+    /**
+     * post 方式发送http请求.
+     *
+     * @param strUrl
+     * @param reqData
+     * @return
+     */
+    public static byte[] doPost(String strUrl,byte[] reqData){
+        return send(strUrl,POST,reqData);
+    }
+
+    /**
+     * get方式发送http请求.
+     *
+     * @param strUrl
+     * @return
+     */
+    public static byte[] doGet(String strUrl){
+        return send(strUrl,GET,null);
+    }
+
+    public static byte[] send(String strUrl,String reqmethod,byte[] reqData){
+        try{
+            URL url = new URL(strUrl);
+            HttpURLConnection httpcon = (HttpURLConnection) url.openConnection();
+            httpcon.setDoOutput(true);  //允许发送数据（输出）
+            httpcon.setDoInput(true);  //允许接收数据（输入）
+            httpcon.setUseCaches(false);  //不使用缓存
+            httpcon.setConnectTimeout(CONN_TIMEOUT); //链接超时时间
+            httpcon.setReadTimeout(READ_TIMEOUT);  //读取超时时间
+            httpcon.setRequestMethod(reqmethod);  //设置请求方法
+            httpcon.connect(); //建立与服务器链接
+            //reqmethod.equalsIgnoreCase(POST)忽略大小写比较
+            if (reqmethod.equalsIgnoreCase(POST)){
+                OutputStream os = httpcon.getOutputStream();
+                os.write(reqData);
+                os.flush();
+                os.close();
+            }
+            BufferedReader in = new BufferedReader(new InputStreamReader(httpcon.getInputStream(),"utf-8"));
+            String inputLine;
+            StringBuilder bankXmlBuffer = new StringBuilder();
+            while ((inputLine = in.readLine()) != null){
+                bankXmlBuffer.append(inputLine);
+            }
+            in.close();
+            httpcon.disconnect();
+            return bankXmlBuffer.toString().getBytes();
+        }catch (Exception ex){
+            log.error(ex.toString(),ex);
+            return null;
+        }
+    }
+
+    public static byte[] readInputStream(InputStream inStream) throws Exception{
+        ByteArrayOutputStream outStream = new ByteArrayOutputStream();
+        byte[] buffer = new byte[1024];
+        int len = 0;
+        while((len = inStream.read(buffer)) != -1){
+            outStream.write(buffer,0,len);
+        }
+        byte[] data = outStream.toByteArray();
+        outStream.close();
+        inStream.close();
+        return data;
+    }
+}
